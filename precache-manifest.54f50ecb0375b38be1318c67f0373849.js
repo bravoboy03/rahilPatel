1e115bbef94e87129add1f659bc5edac40a8a021
Rahil Patel <rahilpatel@Rahils-MacBook-Pro.local>
2023-01-01 17:48:15 -0500

    Updates

---
+++ precache-manifest.54f50ecb0375b38be1318c67f0373849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f939c2c4b40e41eb7e9b",
+    "url": "/rvpResume/static/css/main.649f7dfd.chunk.css"
+  },
+  {
+    "revision": "f939c2c4b40e41eb7e9b",
+    "url": "/rvpResume/static/js/main.f939c2c4.chunk.js"
+  },
+  {
+    "revision": "2fa31fbc0fce2e33cdcb",
+    "url": "/rvpResume/static/js/1.2fa31fbc.chunk.js"
+  },
+  {
+    "revision": "839789071e79129cc182",
+    "url": "/rvpResume/static/js/runtime~main.83978907.js"
+  },
+  {
+    "revision": "0b9577890e5b9eb3b5b3afda666f6ae2",
+    "url": "/rvpResume/static/media/download.0b957789.png"
+  },
+  {
+    "revision": "ba4b65d3b328768e4cd552ac0fc540de",
+    "url": "/rvpResume/index.html"
+  }
+];
